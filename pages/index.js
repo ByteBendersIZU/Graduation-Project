dcc1e37ccb5161144706a78d75376183b3920f37
furkanlebit7 <furkanlebitt7@gmail.com>
2023-01-08 19:07:53 +0300

    packages added

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,9 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Bitirme</>;
+  return (
+    <>
+      <p className="text-red-700">Bitirme</p>
+    </>
+  );
 }
