83c87397c09303032e58aeef93ccde4e9843ed65
furkanlebit7 <furkanlebitt7@gmail.com>
2023-02-23 14:44:15 +0300

    last updates

--- components/menus.js
+++ components/menus.js
@@ -7,7 +7,7 @@ const Menus = () => {
   console.log("session", session);
   // console.log("user", data.session.user.sub);
   return (
-    <div className="bg-orange-300 w-2/12 absolute h-screen top-0">
+    <div className="bg-orange-300 w-2/12 absolute h-screen top-0 z-10">
       {session.data.session.user.sub}
     </div>
   );
