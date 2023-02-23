ba781e490e63555223b9c14aa3ceb64d23e88a1f
furkanlebit7 <furkanlebitt7@gmail.com>
2023-02-23 19:36:02 +0300

    list gessions

--- pages/dashboard/distributor/list.jsx
+++ pages/dashboard/distributor/list.jsx
@@ -1,13 +1,22 @@
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+
 import React from "react";
 
-const List = () => {
-  const session = useSession();
-  console.log("session", session);
-
+const List = (props) => {
+  // const session = useSession();
+  console.log(props);
   return <div>List</div>;
 };
 
+List.auth = true;
+
 export default List;
 
-List.auth = true;
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
+};
